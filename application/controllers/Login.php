903568eb450b2eb522f5891a14685e272b4236ea
William Richard Mangoni <willrpg@hotmail.com>
2016-07-09 20:44:52 -0300

    ajuste de configuração de session

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -2,9 +2,11 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 	class Login extends MY_Controller {
+		
 		function __construct() {
 			parent::__construct();
 		}
+
 		function index() {
 	
 			// VALIDATION RULES
